5822981046d739dce45c69648d6a144bc99c5a4c
shahina9060 <141648831+shahina9060@users.noreply.github.com>
2025-07-28 14:08:15 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,6 @@ const { Sequelize } = require("sequelize");
 const dotenv = require('dotenv')
 dotenv.config()
 
-// console.log("Database:", process.env.database);
-// console.log("Username:", process.env.username);
-// console.log("Password:", process.env.password);
-
-// const sequelize = new Sequelize("messaging_app", "root", "root", {
 const sequelize = new Sequelize(process.env.database, process.env.user_name, process.env.password, {
   host: "localhost",
   dialect: "mysql",
